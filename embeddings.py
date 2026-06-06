075c4655918df1adcd7e1695b804864ba3d21899
Prabhu-449 <pprabhushankar22@gmail.com>
2026-06-06 15:34:23 +0530

    Add files via upload

---
+++ embeddings.py
@@ -0,0 +1,16 @@
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+def create_vectorstore(chunks):
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vectorstore = FAISS.from_texts(
+        texts=chunks,
+        embedding=embeddings
+    )
+
+    return vectorstore
